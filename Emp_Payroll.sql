b5d8e7710d8a57b12abd65f78be08a5ed3cdef24
Tushar Anand <tusharranand@gmail.com>
2022-04-18 19:10:24 +0530

    [Tushar Anand] Added: Ability to create a new table

--- Emp_Payroll.sql
+++ Emp_Payroll.sql
@@ -3,3 +3,9 @@
 create database Payroll_Service
 use Payroll_Service
 
+create table Employee_Payroll(
+Emp_ID int identity(1,1) not null Primary key,
+Name varchar(50),
+Salary bigint,
+StartDate datetime
+)
